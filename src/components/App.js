d13fa48fef289db7db62fdd39aebfd27e3b2fd93
Artem Ponamarchuk <panfil0k@yandex.ru>
2022-10-28 16:45:38 +0400

    fix: error correction

--- src/components/App.js
+++ src/components/App.js
@@ -125,7 +125,7 @@ function App() {
   function handleCardDelete(card) {
     api.deleteCard(card._id)
       .then(() => {
-        setCards((state) => state.filter(item => item._id != card._id))
+        setCards((state) => state.filter(item => item._id !== card._id))
       })
       .catch((err) => {
         console.log(`Ошибка: ${err}`);
